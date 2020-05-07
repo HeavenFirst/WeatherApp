b5df49b330afe6621ae32193ad7fe8e4753ef1a3
Дима Шостак <Shostak.Dmitriy@yahoo.com>
2020-05-07 17:05:32 +0300

    Not working

---
+++ WeatherApp/WeatherApp/ViewModels/CurrentWeatherViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using WeatherApp.Views;
+using Xamarin.Forms;
+
+namespace WeatherApp.ViewModels
+{
+    class CurrentWeatherViewModel : INotifyPropertyChanged
+    {
+        public Command GoToCityEnteringCommand { get; }
+        string city;
+        public string City
+        {
+            get => city;
+            set
+            {
+                city = value;
+                var args = new PropertyChangedEventArgs(nameof(City));
+                PropertyChanged?.Invoke(this, args);
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public CurrentWeatherViewModel()
+        {
+            GoToCityEnteringCommand = new Command(async () =>
+            {
+                var enteringPageVM = new EnteringPageViewModel();
+                var currentweatherVM = new CurrentWeatherPage();
+                currentweatherVM.BindingContext = enteringPageVM;
+                await Application.Current.MainPage.Navigation.PushModalAsync(currentweatherVM);
+            });
+        }
+    }
+}
