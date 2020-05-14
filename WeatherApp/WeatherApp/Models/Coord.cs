f6bafaf3c44ac4fd113c19fe3912501e30d2934a
Дима Шостак <Shostak.Dmitriy@yahoo.com>
2020-05-14 18:02:37 +0300

    Modified Models folder

---
+++ WeatherApp/WeatherApp/Models/Coord.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApp.Models
+{
+    public class Coord
+    {
+        public float lon { get; set; }
+        public float lat { get; set; }
+    }
+}
